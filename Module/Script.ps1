ddbb4b4339a34f0373501f9bb1b76f4ef7090530
FallenGameR <Alex.Kostikov@gmail.com>
2012-12-04 22:04:45 -0800

    todo added

--- Module/Script.ps1
+++ Module/Script.ps1
@@ -3,3 +3,12 @@ $env:path += ";$([Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirecto
 csc.exe /nologo /optimize /out:parser.exe '/lib:d:\Archive\Projects\AntlrAutomation\Libraries\antlr-3.4.1.9004\' /reference:Antlr3.Runtime.dll *.cs .\Properties\AssemblyInfo.cs
 copy d:\Archive\Projects\AntlrAutomation\Libraries\antlr-3.4.1.9004\Antlr3.Runtime.dll .\Antlr3.Runtime.dll
 .\parser.exe
+
+
+<#
+Build dll
+Return tree from it
+dll code should span one single file
+
+all referenced dlls must be embedded as a resource
+#>
