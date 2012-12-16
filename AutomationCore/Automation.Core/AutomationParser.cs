8a80badadf192f8ffd25e422c0f006e5faf74ad9
FallenGameR <Alex.Kostikov@gmail.com>
2012-12-15 22:48:54 -0800

    Added stubs for custom parser, lexer and tree used in grammar.

---
+++ AutomationCore/Automation.Core/AutomationParser.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Parser.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Automation.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Antlr.Runtime;
+
+    public abstract class AutomationParser : Parser
+    {
+        public AutomationParser(ITokenStream input)
+            : base(input)
+        {
+        }
+
+        public AutomationParser(ITokenStream input, RecognizerSharedState state)
+            : base(input, state)
+        {
+        }
+    }
+}
---
+++ AutomationCore/Automation.Core/AutomationTree.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AutomationTree.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Automation.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Antlr.Runtime.Tree;
+    using Antlr.Runtime;
+
+    public class AutomationTree : CommonTree
+    {
+        public AutomationTree()
+            : base()
+        {
+        }
+
+        public AutomationTree(IToken token)
+            : base(token)
+        {
+        }
+
+        public AutomationTree(CommonTree node)
+            : base(node)
+        {
+        }
+    }
+}
