5cbffbbd6d8149d063c91141d28924688777a2b8
FallenGameR <Alex.Kostikov@gmail.com>
2012-12-16 00:04:35 -0800

    Added implementation notes.

--- Module/ParserGenerator.ps1
+++ Module/ParserGenerator.ps1
@@ -40,28 +40,101 @@ function Test-Parser
     [AppDomain]::Unload($parserDomain)
 
     $tree.ToStringTree()
+    #$tree.Children[0].Children
 }
-#$tree.Children[0].Children
 
+
+function Get-Grammar
+{
+    <#
+    .ABSTRACT
+        Get grammar object for specified grammar
+
+    .DESCRIPTION
+        Object properties:
+        - Name
+        - Folder
+        - Assembly
+        - FullText
+        - ShortText
+        + Parse() that can handle AppDomain reuse in filter scenario
+        + Save() that dumps dll and all references to a folder
+    #>
+}
+
+function Set-Grammar
+{
+    <#
+    .ABSTRACT
+        Defines grammar object by grammar text
+
+    .DESCRIPTION
+        Should operate on both full and short text definitions.
+        -Force switch to overwrite existing grammars.
+        - Would search for referenced tokens and grammar name.
+        - Would handle compilation and cleanup.
+        - Would handle md5 grammar text check.
+        - Would handle namespace collisions.
+    #>
+}
+
+filter Parse-Item
+{
+    <#
+    .ABSTRACT
+        Parse file(s) with a grammar
+
+    .DESCRIPTION
+        Takes grammar name or full/short text to construct AST for each input file.
+        -Parallel runs parsing in parallel threads
+        -Tee would output input files to console and return AST as usual
+
+        Alias: Parse-File
+    #>
+}
+
+filter Get-Node
+{
+    <#
+    .ABSTRACT
+        Get corresponding node(s) in the AST
+
+    .DESCRIPTION
+        Finds all nodes with name specified via regex.
+        -Recurse would traverse whole input tree
+        -OnlyChildren would return only children, without returning matching nodes themselves
+        -OnlyTokens would return only matching token names, without returning the values
+
+        Can accept multiple nodes as input.
+
+        Alias: Get-Tree, Where-Tree, Where-Node
+
+        Uses methods defined in AutomationTree. That way we could use the same Get-Node functionality from C# project that uses the same genrated parser.
+    #>
+}
 
 
 <#
-Multiple parsers can be generated in the same Powershell session.
+Render trees simplified - text + children
 
-Source files are taken from the module folder itself.
+Allow syntax like: FILE.some.sub.child (expand for all found children, discard the onces that don not have this path)?
 
-#Render trees simplified - text + children
-#Allow syntax like:
-#- get all 'FILE' children +recursive
-#- FILE.some.sub.child (expand for all found children, discard the onces that don not have this path)?
+Predefined samples:
+- ini
+- csv
+- ping
+- tracert
+- whois
+- diff?
+- git?
+- sd?
+- logs?
+- cs? (fuzzy)
 
-Automatic temp file cleanup.
-
-Parser caching.
-
-Simplified grammars on input.
-
-Grammar error handling.
-
-Predefined grammars for fuzzy cs filter, csv and ini.
+Error handling:
+- missing/extra token that is treated as recoverable error is a warning
+- parser error recovery via "recovery stack" is treated as an error
+- NoViableAltExceptions is as much verbose as possible
+- Compilation warnings to warnings
+- Compilation errors to errors. The grammar is discarded since it is not valid.
 #>
