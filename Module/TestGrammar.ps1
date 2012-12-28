859db4ab3a1635544af8e09d779371679fd298e5
FallenGameR <Alex.Kostikov@gmail.com>
2012-12-27 23:53:57 -0800

    Added test plan.

--- Module/TestGrammar.ps1
+++ Module/TestGrammar.ps1
@@ -1,3 +1,10 @@
+
+# Simpleton grammar compiles without errors
+# Parse-Item works with exact grammar name
+# Parse-Item works with exact grammar name case insensetive
+# Parse-Item works with regex matched grammar name
+# Parse-Item works with regex matched grammar name with warning in case of ambiguity
+#
 $fullText = type "$PSScriptRoot\..\Info\simpleton.g3" | Out-String
 Set-Grammar $fullText
 "$PSScriptRoot\..\Info\simpleton.txt" | Parse-Item simple
