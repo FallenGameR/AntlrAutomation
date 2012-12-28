078d7cbb52fe327095b67730647c7ee1a7f523cd
FallenGameR <Alex.Kostikov@gmail.com>
2012-12-28 00:00:06 -0800

    Implemented tests.

--- Module/Parsers/Simpleton/src/SimpletonLexer.cs
+++ Module/Parsers/Simpleton/src/SimpletonLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.4.1.9004 D:\\Archive\\Projects\\AntlrAutomation\\Module\\Parsers\\Simpleton\\Simpleton.g3 2012-12-27 23:50:30
+// $ANTLR 3.4.1.9004 D:\\Archive\\Projects\\AntlrAutomation\\Module\\Parsers\\Simpleton\\Simpleton.g3 2012-12-27 23:59:51
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
--- Module/TestGrammar.ps1
+++ Module/TestGrammar.ps1
@@ -1,11 +1,17 @@
-
 # Simpleton grammar compiles without errors
-# Parse-Item works with exact grammar name
-# Parse-Item works with exact grammar name case insensetive
-# Parse-Item works with regex matched grammar name
-# Parse-Item works with regex matched grammar name with warning in case of ambiguity
-#
 $fullText = type "$PSScriptRoot\..\Info\simpleton.g3" | Out-String
 Set-Grammar $fullText
+
+# Parse-Item works with exact grammar name
+"$PSScriptRoot\..\Info\simpleton.txt" | Parse-Item Simpleton
+
+# Parse-Item works with exact grammar name case insensetive
+"$PSScriptRoot\..\Info\simpleton.txt" | Parse-Item simpleton
+
+# Parse-Item works with regex matched grammar name
 "$PSScriptRoot\..\Info\simpleton.txt" | Parse-Item simple
 
+# Parse-Item works with regex matched grammar name with warning in case of ambiguity
+$fullTextCopy = type "$PSScriptRoot\..\Info\simpletonCopy.g3" | Out-String
+Set-Grammar $fullTextCopy
+"$PSScriptRoot\..\Info\simpleton.txt" | Parse-Item simple
