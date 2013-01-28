d48535ec26ec54c2a86dd565bf1ba79e1086c338
Alexander Kostikov <alexko@microsoft.com>
2013-01-27 22:50:46 -0800

    Added test project.

---
+++ AutomationCore/Automation.Core.Tests/IndentionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Automation.Core.Tests
+{
+    [TestClass]
+    public class IndentionsTests
+    {
+        [TestMethod]
+        public void Indentions_are_defined()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
