0655d295aea02ba5226d4a20e18f79bca7ce6ba3
Alexander Kostikov <alexko@microsoft.com>
2013-01-29 23:00:07 -0800

    Simple dynamic test.

--- AutomationCore/Automation.Core.Tests/DynamicTests.cs
+++ AutomationCore/Automation.Core.Tests/DynamicTests.cs
@@ -14,157 +14,36 @@ namespace Automation.Core.Tests
     public class DynamicTests
     {
         [TestMethod]
-        public void Test()
+        public void DynamicTest()
         {
-            dynamic dict = new DynamicDictionary();
-
-            Console.WriteLine(dict.First.ToString());
-
-            dict.SetDictionaryEntry("test", "me");
-            Console.WriteLine(dict.GetDictionaryEntry("test"));
-            dict.WriteMethodInfo("method info");
-            Console.WriteLine(dict.ToString());
+            dynamic obj = new DynamicObject();
+            Assert.AreEqual("text", obj.SomeText);
         }
     }
 
-    public class DynamicDictionary : IDynamicMetaObjectProvider
+    public class DynamicObject : IDynamicMetaObjectProvider
     {
-        private Dictionary<string, object> storage = new Dictionary<string, object>();
-        private int[] elements = new int[] { 1, 2 };
-
-        public object GetFirst()
-        {
-            return elements[0];
-        }
-
-        public object GetSecond()
-        {
-            return elements[1];
-        }
-
-        public object SetDictionaryEntry(string key, object value)
-        {
-            if (storage.ContainsKey(key))
-                storage[key] = value;
-            else
-                storage.Add(key, value);
-            return value;
-        }
-
-        public object GetDictionaryEntry(string key)
-        {
-            object result = null;
-            if (storage.ContainsKey(key))
-            {
-                result = storage[key];
-            }
-            return result;
-        }
-
-        public object WriteMethodInfo(string methodInfo)
-        {
-            Console.WriteLine(methodInfo);
-            return 42; // because it is the answer to everything
-        }
-
-        public override string ToString()
-        {
-            StringWriter message = new StringWriter();
-            foreach (var item in storage)
-                message.WriteLine("{0}:\t{1}", item.Key, item.Value);
-            return message.ToString();
-        }
-
         public DynamicMetaObject GetMetaObject(Expression parameter)
         {
-            return new Helper(parameter, this);
+            return new DynamicMeta(parameter, this);
+        }
+    }
+
+    public class DynamicMeta : DynamicMetaObject
+    {
+        public DynamicMeta(Expression parameter, DynamicObject value)
+            : base(parameter, BindingRestrictions.Empty, value)
+        {
         }
 
-        private class Helper : DynamicMetaObject
+        public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
         {
-            public Helper(Expression parameter, DynamicDictionary value)
-                : base(parameter, BindingRestrictions.Empty, value)
-            {
-            }
+            var methodName = "GetDictionaryEntry";
 
-            public override DynamicMetaObject BindSetMember(SetMemberBinder binder, DynamicMetaObject value)
-            {
-                // Method to call in the containing class:
-                string methodName = "SetDictionaryEntry";
-
-                // setup the binding restrictions.
-                var restrictions = BindingRestrictions.GetTypeRestriction(Expression, LimitType);
-
-                // setup the parameters:
-                var args = new Expression[2];
-                // First parameter is the name of the property to Set
-                args[0] = Expression.Constant(binder.Name);
-                // Second parameter is the value
-                args[1] = Expression.Convert(value.Expression, typeof(object));
-
-                // Setup the 'this' reference
-                var self = Expression.Convert(Expression, LimitType);
-
-                // Setup the method call expression
-                var methodCall = Expression.Call(self, typeof(DynamicDictionary).GetMethod(methodName), args);
-
-                // Create a meta object to invoke Set later:
-                var setDictionaryEntry = new DynamicMetaObject(methodCall, restrictions);
-
-                Console.WriteLine("Set");
-
-                // return that dynamic object
-                return setDictionaryEntry;
-            }
-
-            public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
-            {
-                // Method call in the containing class:
-                var methodName = "GetDictionaryEntry";
-
-                // One parameter
-                var parameters = new []
-                {
-                    Expression.Constant(binder.Name)
-                };
-
-                var getDictionaryEntry = new DynamicMetaObject(
-                    Expression.Call(
-                        Expression.Convert(Expression, LimitType),
-                        typeof(DynamicDictionary).GetMethod(methodName),
-                        parameters),
-                    BindingRestrictions.GetTypeRestriction(Expression, LimitType));
-
-                Console.WriteLine("Get");
-
-                return getDictionaryEntry;
-            }
-
-            public override DynamicMetaObject BindInvokeMember(InvokeMemberBinder binder, DynamicMetaObject[] args)
-            {
-                var paramInfo = new StringBuilder();
-
-                paramInfo.AppendFormat("Calling {0}(", binder.Name);
-                foreach (var item in args)
-                    paramInfo.AppendFormat("{0}, ", item.Value);
-                paramInfo.Append(")");
-
-                var parameters = new []
-                {
-                    Expression.Constant(paramInfo.ToString())
-                };
-
-                var methodInfo = new DynamicMetaObject(
-                    Expression.Call(
-                    Expression.Convert(Expression, LimitType),
-                    typeof(DynamicDictionary).GetMethod("WriteMethodInfo"),
-                    parameters),
-                    BindingRestrictions.GetTypeRestriction(Expression, LimitType));
-
-                Console.WriteLine("Call");
-
-                return methodInfo;
-            }
+            return new DynamicMetaObject(
+                Expression.Convert(Expression.Constant("text"), typeof(string)),
+                BindingRestrictions.GetExpressionRestriction(Expression.Constant(true)));
+                    //BindingRestrictions.GetTypeRestriction(this.Expression, this.LimitType));
         }
     }
 }
