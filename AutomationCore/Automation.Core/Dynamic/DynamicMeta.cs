fc3b67aefd2c2ef2fab71467a37812eac635a97c
Alexander Kostikov <alexko@microsoft.com>
2013-01-29 23:25:39 -0800

    By default AutomationTree passes control to base binder.

---
+++ AutomationCore/Automation.Core/Dynamic/DynamicMeta.cs
@@ -0,0 +1,28 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+
+namespace Automation.Core
+{
+    public class DynamicMeta : DynamicMetaObject
+    {
+        public DynamicMeta(Expression parameter, object value)
+            : base(parameter, BindingRestrictions.Empty, value)
+        {
+        }
+
+        public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
+        {
+            if (binder.Name == "SomeText")
+            {
+                return new DynamicMetaObject(
+                    Expression.Convert(Expression.Constant("text"), typeof(string)),
+                    BindingRestrictions.GetExpressionRestriction(Expression.Constant(true)));
+                //BindingRestrictions.GetTypeRestriction(this.Expression, this.LimitType));
+            }
+            else
+            {
+                return base.BindGetMember(binder);
+            }
+        }
+    }
+}
