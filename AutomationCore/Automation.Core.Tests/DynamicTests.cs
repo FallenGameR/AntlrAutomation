78c8afc461ce56efb0697a15bde6b750e2dac98e
FallenGameR <Alex.Kostikov@gmail.com>
2013-01-30 23:51:30 -0800

    Dumped properties to figure out how to populate a node to make test more comprehensive.

--- AutomationCore/Automation.Core.Tests/DynamicTests.cs
+++ AutomationCore/Automation.Core.Tests/DynamicTests.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr.Runtime;
+using Antlr.Runtime.Tree;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Automation.Core.Tests
@@ -16,11 +18,25 @@ namespace Automation.Core.Tests
         [TestMethod]
         public void Static_properties_can_be_accessed_with_case_insensitive_names()
         {
+            var node = new AutomationTree(new CommonToken());
+            //node.CharPositionInLine
+            //node.ChildCount
+            //node.ChildIndex
+            //node.Children
+            //node.IsNil
+            //node.Line
+            //node.Parent
+            //node.Text
+            //node.Token
+            //node.TokenStartIndex
+            //node.TokenStopIndex
+            //node.Type
+
             Assert.Inconclusive();
         }
 
         [TestMethod]
-        public void Dynamic_properties_return_children_with_corresponding_name()
+        public void Dynamic_properties_return_children_with_corresponding_case_insensitive_name()
         {
             Assert.Inconclusive();
         }
