5ab99abc539b10bbf9f3729d5054b38839ef9e51
FallenGameR <Alex.Kostikov@gmail.com>
2013-02-04 00:05:43 -0800

    Failing powershell tests added.

---
+++ AutomationCore/Automation.Module.Tests/PowershellTests.cs
@@ -0,0 +1,49 @@
+﻿using Automation.Module.Tests.TestUtils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Automation.Module.Tests
+{
+    [TestClass]
+    public class PowershellTests
+    {
+        [TestMethod]
+        public void Powershell_stdout_is_returned_sucessfully()
+        {
+            var actual = Powershell.Script("'hello world'");
+            var expected = "hello world";
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, Powershell.Out);
+            Assert.IsTrue(string.IsNullOrEmpty(Powershell.Err));
+        }
+
+        [TestMethod]
+        public void Powershell_stderr_is_returned_sucessfully()
+        {
+            // net use xxx
+            var actual = Powershell.Script("[Console]::Error.WriteLine('test')");
+            var expected = "test";
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, Powershell.Err);
+            Assert.IsTrue(string.IsNullOrEmpty(Powershell.Out));
+        }
+
+        [TestMethod]
+        public void Powershell_write_error_is_returned_sucessfully()
+        {
+            var actual = Powershell.Script("Write-Error 'message'");
+            var expected = "message";
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, Powershell.Err);
+        }
+
+        [TestMethod]
+        public void Powershell_throw_is_returned_sucessfully()
+        {
+            var actual = Powershell.Script("throw 'info'");
+            var expected = "info";
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, Powershell.Out);
+            Assert.IsTrue(string.IsNullOrEmpty(Powershell.Out));
+        }
+    }
+}
---
+++ AutomationCore/Automation.Module.Tests/TestUtils/Powershell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Module.Tests.TestUtils
+{
+    public class Powershell
+    {
+        private Powershell()
+        {
+
+        }
+
+        public static Powershell GetInstance()
+        {
+            return new Powershell();
+        }
+
+        internal static string Script(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Out { get; set; }
+
+        public static string Err { get; set; }
+    }
+}
