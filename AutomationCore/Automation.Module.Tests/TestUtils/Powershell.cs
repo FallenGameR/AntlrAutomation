44143c680fcc72a84c0f5776da13bdcc57729a61
FallenGameR <Alex.Kostikov@gmail.com>
2013-02-04 20:26:46 -0800

    Powershell class implementation.

--- AutomationCore/Automation.Module.Tests/TestUtils/Powershell.cs
+++ AutomationCore/Automation.Module.Tests/TestUtils/Powershell.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Automation.Module.Tests.TestUtils
@@ -10,10 +9,24 @@ namespace Automation.Module.Tests.TestUtils
     public sealed class Powershell: IDisposable
     {
         private readonly string scriptPath;
+        private readonly ProcessStartInfo startInfo;
+        private Process process;
+        private BlockingCollection<string> allLines;
+        private BlockingCollection<string> outLines;
+        private BlockingCollection<string> errLines;
 
         private Powershell()
         {
             this.scriptPath = GetTempScriptPath();
+            this.startInfo = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = string.Format("-NoProfile -ExecutionPolicy RemoteSigned -File \"{0}\"", this.scriptPath),
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
         }
 
         public static string Out { get; private set; }
@@ -43,9 +56,63 @@ namespace Automation.Module.Tests.TestUtils
 
         public string Execute(string script)
         {
+            // TODO: Add module import
+
+            // Save script content
             File.WriteAllText(this.scriptPath, script);
-            // Write to temp file, call, process out and err
-            return null;
+
+            // All lines as they arrive
+            this.allLines = new BlockingCollection<string>();
+            this.outLines = new BlockingCollection<string>();
+            this.errLines = new BlockingCollection<string>();
+
+            // Start powershell process that alls script
+            using (this.process = Process.Start(this.startInfo))
+            using (var taskOut = Task.Factory.StartNew(this.OutputStreamHandler))
+            using (var taskErr = Task.Factory.StartNew(this.ErrorsStreamHandler))
+            {
+                try
+                {
+                    this.process.WaitForExit();
+                    Task.WaitAll(taskOut, taskErr);
+                }
+                finally
+                {
+                    this.outLines.CompleteAdding();
+                    this.errLines.CompleteAdding();
+                    this.allLines.CompleteAdding();
+                }
+            }
+
+            Out = string.Join(Environment.NewLine, this.outLines.ToArray());
+            Err = string.Join(Environment.NewLine, this.errLines.ToArray());
+            return string.Join(Environment.NewLine, this.allLines.ToArray());
+        }
+
+        private void OutputStreamHandler()
+        {
+            var line = this.process.StandardOutput.ReadLine();
+
+            while (line != null)
+            {
+                this.allLines.Add(line);
+                this.outLines.Add(line);
+                Console.WriteLine("out: {0}", line);
+                line = this.process.StandardOutput.ReadLine();
+            }
+        }
+
+        private void ErrorsStreamHandler()
+        {
+            var line = this.process.StandardError.ReadLine();
+
+            while (line != null)
+            {
+                this.allLines.Add(line);
+                this.errLines.Add(line);
+                Console.WriteLine("err: {0}", line);
+                line = this.process.StandardError.ReadLine();
+            }
         }
 
         public void Dispose()
