b3998052f0ddf6d3ed3ffcfaed287dc4ff55578a
FallenGameR <Alex.Kostikov@gmail.com>
2013-02-14 22:17:52 -0800

    Grammar draft.

---
+++ AutomationCore/Automation.Core/Core/Grammar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Antlr.Runtime;
+
+namespace Automation.Core
+{
+    public class Grammar
+    {
+        public string Name { get; private set; }
+        public string Folder { get; private set; }
+        public string Assembly { get; private set; }
+        public string FullText { get { return null; } }
+        public string ShortText { get { return null; } }
+
+        public AutomationTree Parse(string text)
+        {
+            return null;
+        }
+
+        public IToken[] Tokenize(string text)
+        {
+            return null;
+        }
+    }
+}
