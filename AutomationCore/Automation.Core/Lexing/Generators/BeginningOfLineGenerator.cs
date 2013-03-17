72c2818184a49cce554598fda3832352e4780bf3
FallenGameR <Alex.Kostikov@gmail.com>
2013-03-17 16:14:32 -0700

    Renames to better represent project structure.

---
+++ AutomationCore/Automation.Core/Lexing/Generators/BeginningOfLineGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Antlr.Runtime;
+
+namespace Automation.Core
+{
+    public class BeginningOfLineGenerator : IGenerator
+    {
+        public bool IsTrigger(IToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IToken> Generate(IToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
